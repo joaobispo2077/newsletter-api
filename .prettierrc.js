2fff528d12d16f880c4729a9da28595c2e0d8b0e
joaobispo2077 <joaobispo2077@gmail.com>
2023-08-31 10:12:49 -0300

    chore: generate boilerplate aws-nodejs-ecma-script & setup eslint/prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Options} */
+const config = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'lf',
+}
+
+module.exports = config;
