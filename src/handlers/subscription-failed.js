36801d082c89d3584c254c54408a2f06e64f7883
joaobispo2077 <joaobispo2077@gmail.com>
2023-09-07 16:36:47 -0300

    feat: add failed subscription flow

---
+++ src/handlers/subscription-failed.js
@@ -0,0 +1,45 @@
+import { failedMessagesService } from '../services/failed-messages-service';
+
+export const handler = async (event, _context, _callback) => {
+  try {
+    console.info(`Initialized processing of failed messaages from SQS`);
+    const promiseResultList = await Promise.allSettled(
+      event.Records.map(async (record) => {
+        const { messageId } = record;
+
+        return await failedMessagesService.store({
+          id: messageId,
+          resource: 'reader#subscribe',
+          message: JSON.stringify(record),
+        });
+      }),
+    );
+
+    const failedMessageIdList = promiseResultList.reduce(
+      (acc, current, index) => {
+        if (current.status === 'rejected') {
+          console.error(
+            `[${index}] Message rejected by reason: ${current.reason}`,
+          );
+          acc.push(event.Records[index].messageId);
+        }
+
+        return acc;
+      },
+      [],
+    );
+
+    const batchItemFailures = failedMessageIdList.map((failedMessageId) => ({
+      itemIdentifier: failedMessageId,
+    }));
+    console.info(`Finished processing of messaages from SQS`);
+    console.info(`Number of messages with errors: ${batchItemFailures.length}`);
+
+    return {
+      batchItemFailures,
+    };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
