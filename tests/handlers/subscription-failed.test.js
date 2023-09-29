7f7a9b069fc1e836c0d1556c000b8837578acf3f
joaobispo2077 <joaobispo2077@gmail.com>
2023-09-29 11:34:00 -0300

    test: should be able to handle failed subscription properly

---
+++ tests/handlers/subscription-failed.test.js
@@ -0,0 +1,105 @@
+import { handler } from '../../src/handlers/subscription-failed';
+import { failedMessagesService } from '../../src/services/failed-messages-service';
+
+jest.mock('../services/failed-messages-service');
+
+describe('subscription-failed handler', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should store failed messages and return batch item failures', async () => {
+    const event = {
+      Records: [
+        {
+          messageId: '1',
+          body: 'test message 1',
+        },
+        {
+          messageId: '2',
+          body: 'test message 2',
+        },
+      ],
+    };
+
+    const storeMock = jest.fn();
+    failedMessagesService.store.mockImplementation(storeMock);
+
+    const result = await handler(event);
+
+    expect(storeMock).toHaveBeenCalledTimes(2);
+    expect(storeMock).toHaveBeenCalledWith({
+      id: '1',
+      resource: 'reader#subscribe',
+      message: JSON.stringify(event.Records[0]),
+    });
+    expect(storeMock).toHaveBeenCalledWith({
+      id: '2',
+      resource: 'reader#subscribe',
+      message: JSON.stringify(event.Records[1]),
+    });
+    expect(result).toEqual({
+      batchItemFailures: [],
+    });
+  });
+
+  it('should return batch item failures for rejected promises', async () => {
+    const event = {
+      Records: [
+        {
+          messageId: '1',
+          body: 'test message 1',
+        },
+        {
+          messageId: '2',
+          body: 'test message 2',
+        },
+      ],
+    };
+
+    const storeMock = jest.fn();
+    failedMessagesService.store.mockImplementation(storeMock);
+
+    storeMock.mockRejectedValueOnce(new Error('Failed to store message 1'));
+
+    const result = await handler(event);
+
+    expect(storeMock).toHaveBeenCalledTimes(2);
+    expect(storeMock).toHaveBeenCalledWith({
+      id: '1',
+      resource: 'reader#subscribe',
+      message: JSON.stringify(event.Records[0]),
+    });
+    expect(storeMock).toHaveBeenCalledWith({
+      id: '2',
+      resource: 'reader#subscribe',
+      message: JSON.stringify(event.Records[1]),
+    });
+    expect(result).toEqual({
+      batchItemFailures: [
+        {
+          itemIdentifier: '1',
+        },
+      ],
+    });
+  });
+
+  it('should throw an error if failedMessagesService.store throws an error', async () => {
+    const event = {
+      Records: [
+        {
+          messageId: '1',
+          body: 'test message 1',
+        },
+      ],
+    };
+
+    const storeMock = jest.fn();
+    failedMessagesService.store.mockImplementation(storeMock);
+
+    const error = new Error('Failed to store message');
+    storeMock.mockRejectedValueOnce(error);
+
+    await expect(handler(event)).rejects.toThrow(error);
+  });
+});
